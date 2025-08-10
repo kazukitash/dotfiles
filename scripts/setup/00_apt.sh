84d5f8d139a98617f12b4fae50f4c23d21fe5a5f
kazukitash <kazukitash@gmail.com>
2025-08-10 22:37:14 +0900

    style(setup): セットアップスクリプトのインデントを統一
    APTとzshセットアップスクリプトのcase文内のインデントを
    統一されたスタイルに修正

--- scripts/setup/00_apt.sh
+++ scripts/setup/00_apt.sh
@@ -10,41 +10,41 @@ fi
 install_apt() {
   case "$(arch)" in
     Linux)
-    e_header "Prepare" "Install dependencies for Linux"
+      e_header "Prepare" "Install dependencies for Linux"
 
-    e_log "Prepare" "Need root privilege"
-    sudo printf "\e[32;4mPrepare\e[0m \e[32mRoot privilege - ✔  OK\e[0m\n"
-    if [ $? -ne 0 ]; then
-      e_error "Prepare" "Wrong password"
-      e_error "Prepare" "Abort the process"
-      exit 1
-    fi
+      e_log "Prepare" "Need root privilege"
+      sudo printf "\e[32;4mPrepare\e[0m \e[32mRoot privilege - ✔  OK\e[0m\n"
+      if [ $? -ne 0 ]; then
+        e_error "Prepare" "Wrong password"
+        e_error "Prepare" "Abort the process"
+        exit 1
+      fi
 
-    # apt-getでUIを使うインストールを避ける
-    export DEBIAN_FRONTEND=noninteractive
+      # apt-getでUIを使うインストールを避ける
+      export DEBIAN_FRONTEND=noninteractive
 
-    # Localeの設定
-    e_log "Prepare" "Updating apt..."
-    export LC_ALL=C
-    sudo apt-get update
-    check_result $? "Prepare" "Update apt"
+      # Localeの設定
+      e_log "Prepare" "Updating apt..."
+      export LC_ALL=C
+      sudo apt-get update
+      check_result $? "Prepare" "Update apt"
 
-    # タイムゾーンの設定
-    e_log "Prepare" "Installing tzdata..."
-    sudo apt-get install -y tzdata
-    check_result $? "Prepare" "Install tzdata"
-    export TZ=Asia/Tokyo
+      # タイムゾーンの設定
+      e_log "Prepare" "Installing tzdata..."
+      sudo apt-get install -y tzdata
+      check_result $? "Prepare" "Install tzdata"
+      export TZ=Asia/Tokyo
 
-    # 基本パッケージをインストール
-    e_log "Prepare" "Installing base packages..."
-    sudo apt-get install -y build-essential procps curl file wget git
-    check_result $? "Prepare" "Install base packages"
+      # 基本パッケージをインストール
+      e_log "Prepare" "Installing base packages..."
+      sudo apt-get install -y build-essential procps curl file wget git
+      check_result $? "Prepare" "Install base packages"
 
-    # aptのcacheを消す
-    e_log "Prepare" "Cleaning apt..."
-    sudo apt-get clean -y && sudo rm -rf /var/lib/apt/lists/*
-    check_result $? "Prepare" "Cleanup apt"
-    ;;
+      # aptのcacheを消す
+      e_log "Prepare" "Cleaning apt..."
+      sudo apt-get clean -y && sudo rm -rf /var/lib/apt/lists/*
+      check_result $? "Prepare" "Cleanup apt"
+      ;;
   esac
 }
 
--- scripts/setup/03_zsh.sh
+++ scripts/setup/03_zsh.sh
@@ -10,26 +10,26 @@ fi
 setup_zsh() {
   case "$(arch)" in
     Linux)
-    e_header "Setup zsh" "Start setup"
-    ZSH_PATH=$(which zsh)
+      e_header "Setup zsh" "Start setup"
+      ZSH_PATH=$(which zsh)
 
-    e_log "Setup zsh" "Need root privilege"
-    sudo printf "\e[32;4mSetup zsh\e[0m \e[32mRoot privilege - ✔  OK\e[0m\n"
-    if [ $? -ne 0 ]; then
-      e_error "Setup zsh" "Wrong password"
-      e_error "Setup zsh" "Abort the process"
-      exit 1
-    fi
+      e_log "Setup zsh" "Need root privilege"
+      sudo printf "\e[32;4mSetup zsh\e[0m \e[32mRoot privilege - ✔  OK\e[0m\n"
+      if [ $? -ne 0 ]; then
+        e_error "Setup zsh" "Wrong password"
+        e_error "Setup zsh" "Abort the process"
+        exit 1
+      fi
 
-    e_log "Setup zsh" "Adding shells..."
-    sudo sh -c "echo $ZSH_PATH >>/etc/shells"
-    check_result $? "Setup zsh" "Add shells"
+      e_log "Setup zsh" "Adding shells..."
+      sudo sh -c "echo $ZSH_PATH >>/etc/shells"
+      check_result $? "Setup zsh" "Add shells"
 
-    e_log "Setup zsh" "Changing default shell..."
-    chsh -s $ZSH_PATH      # user
-    sudo chsh -s $ZSH_PATH # root
-    check_result $? "Setup zsh" "Change default shell"
-    ;;
+      e_log "Setup zsh" "Changing default shell..."
+      chsh -s $ZSH_PATH      # user
+      sudo chsh -s $ZSH_PATH # root
+      check_result $? "Setup zsh" "Change default shell"
+      ;;
   esac
 }
 
